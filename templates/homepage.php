1e27b7e663598ab28285aa236f186c9b4a094f5d
dtrinidadiv <dtrinidadiv.dev@gmail.com>
2015-08-26 00:48:12 +0800

    tweak homepage

--- templates/homepage.php
+++ templates/homepage.php
@@ -9,12 +9,13 @@
                 </h1>
             	<?php foreach ( $results['articles'] as $article ) { ?>
  					 <h2>
-                    	<a href=".?action=viewArticle&amp;articleId=<?php echo $article->id?>"><?php echo htmlspecialchars( $article->title )?></a>
+                    	<?php echo htmlspecialchars( $article->title )?>
                		 </h2>
+                     <hr>
                		 <p><span class="glyphicon glyphicon-time"></span><?php echo date('j F Y', $article->publicationDate)?></p>
            			 <p class="summary"><?php echo htmlspecialchars( $article->summary )?></p>
-                	 <a class="btn btn-primary" href="#"><span class="glyphicon glyphicon-chevron-right"></span> Read More </a>
-
+                	 <a class="btn btn-primary" href=".?action=viewArticle&amp;articleId=<?php echo $article->id?>"><span class="glyphicon glyphicon-chevron-right"></span> Read More </a>
+                     <hr>
  				<?php } ?>
         
 
